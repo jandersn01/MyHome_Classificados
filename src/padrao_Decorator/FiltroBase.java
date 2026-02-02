fb406da595dc90a533c342f2e1218e4acd8863dc
vtrnduda <vtrnduda@gmail.com>
2026-02-01 23:06:26 -0300

    feat: implementa o padrão decorator

--- src/padrao_Decorator/FiltroBase.java
+++ src/padrao_Decorator/FiltroBase.java
@@ -4,18 +4,29 @@ import java.util.List;
 
 import model.anuncio.Anuncio;
 
-public class FiltroBase implements ComponentBusca {
-	
-	public ComponentBusca buscaDecorada;
-	
-	@Override
-	public List<Anuncio> exercutarBusca() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	public FiltroBase(ComponentBusca busca) {
-		//TODO
-	}
+/**
+ * Decorator abstrato base para filtros.
+ * RF06: Novos filtros podem ser adicionados dinamicamente.
+ */
+public abstract class FiltroBase implements ComponentBusca {
 
+    protected ComponentBusca buscaDecorada;
+
+    public FiltroBase(ComponentBusca busca) {
+        this.buscaDecorada = busca;
+    }
+
+    @Override
+    public List<Anuncio> executarBusca() {
+        // Executa a busca decorada e aplica o filtro específico
+        List<Anuncio> resultados = buscaDecorada.executarBusca();
+        return filtrar(resultados);
+    }
+
+    /**
+     * Método abstrato que aplica o filtro específico.
+     * @param anuncios Lista de anúncios a filtrar
+     * @return Lista filtrada
+     */
+    protected abstract List<Anuncio> filtrar(List<Anuncio> anuncios);
 }
--- src/padrao_Decorator/FiltroPreco.java
+++ src/padrao_Decorator/FiltroPreco.java
@@ -1,20 +1,52 @@
 package padrao_Decorator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import model.anuncio.Anuncio;
 
+/**
+ * Decorator concreto - Filtra por faixa de preço.
+ * RF06: Usuários podem buscar por faixa de preço.
+ */
 public class FiltroPreco extends FiltroBase {
 
-	public FiltroPreco(ComponentBusca busca) {
-		super(busca);
-		// TODO Auto-generated constructor stub
-	}
+    private double precoMinimo;
+    private double precoMaximo;
 
-	@Override
-	public List<Anuncio> exercutarBusca() {
-		// TODO Auto-generated method stub
-		return super.exercutarBusca();
-	}
+    public FiltroPreco(ComponentBusca busca, double precoMinimo, double precoMaximo) {
+        super(busca);
+        this.precoMinimo = precoMinimo;
+        this.precoMaximo = precoMaximo;
+    }
 
+    @Override
+    protected List<Anuncio> filtrar(List<Anuncio> anuncios) {
+        List<Anuncio> filtrados = new ArrayList<>();
+
+        for (Anuncio anuncio : anuncios) {
+            Double preco = anuncio.getPreco();
+            if (preco != null && preco >= precoMinimo && preco <= precoMaximo) {
+                filtrados.add(anuncio);
+            }
+        }
+
+        return filtrados;
+    }
+
+    public double getPrecoMinimo() {
+        return precoMinimo;
+    }
+
+    public void setPrecoMinimo(double precoMinimo) {
+        this.precoMinimo = precoMinimo;
+    }
+
+    public double getPrecoMaximo() {
+        return precoMaximo;
+    }
+
+    public void setPrecoMaximo(double precoMaximo) {
+        this.precoMaximo = precoMaximo;
+    }
 }
