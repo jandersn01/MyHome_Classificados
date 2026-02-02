a68da62157245c3eb7d34ce45f0aa90c3c998933
vtrnduda <vtrnduda@gmail.com>
2026-02-01 23:08:29 -0300

    feat: adiciona dados em csv, logs, models, etc

--- src/model/imovel/SalaComercial.java
+++ src/model/imovel/SalaComercial.java
@@ -1,17 +1,51 @@
 package model.imovel;
 
-public class SalaComercial extends Imovel{
+public class SalaComercial extends Imovel {
 
-	@Override
-	public Imovel clone() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    private int capacidadePessoas;
+    private boolean estacionamento;
 
-	@Override
-	public String getTipoImovel() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public SalaComercial() {
+        super();
+    }
 
+    public SalaComercial(String titulo, double preco, String descricao, String localizacao,
+                         double area, int capacidadePessoas, boolean estacionamento) {
+        super(titulo, preco, descricao, localizacao, area);
+        this.capacidadePessoas = capacidadePessoas;
+        this.estacionamento = estacionamento;
+    }
+
+    // Construtor de cópia
+    public SalaComercial(SalaComercial outra) {
+        super(outra);
+        this.capacidadePessoas = outra.capacidadePessoas;
+        this.estacionamento = outra.estacionamento;
+    }
+
+    public int getCapacidade() {
+        return capacidadePessoas;
+    }
+
+    public void setCapacidadePessoas(int capacidadePessoas) {
+        this.capacidadePessoas = capacidadePessoas;
+    }
+
+    public boolean temEstacionamento() {
+        return estacionamento;
+    }
+
+    public void setEstacionamento(boolean estacionamento) {
+        this.estacionamento = estacionamento;
+    }
+
+    @Override
+    public String getTipoImovel() {
+        return "Sala Comercial";
+    }
+
+    @Override
+    public Imovel clone() {
+        return new SalaComercial(this);
+    }
 }
